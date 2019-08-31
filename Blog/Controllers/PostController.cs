9bcd6a50488b26e3ecf58ddc4efc63e33c8126f7
Ahmed Mohamed <ahmed.m3.abdallah@gmail.com>
2019-08-31 15:22:08 +0200

    Add feature: can delete post

--- Blog/Controllers/PostController.cs
+++ Blog/Controllers/PostController.cs
@@ -19,8 +19,9 @@ namespace Blog.Controllers
             _context = new ApplicationDbContext();
             userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(this._context));
         }
-        
+
         // GET: Post
+        [Authorize]
         public ActionResult Index()
         {
             var posts = _context.Posts.OrderByDescending(p => p.Created_at).ToList();
@@ -47,7 +48,8 @@ namespace Blog.Controllers
         public ActionResult Detail(int id)
         {
             Post post = _context.Posts.Find(id);
-            return View(post);
+
+            return post.User.Id == User.Identity.GetUserId() ? View("OwnerDetail", post) : View(post);
         }
 
         public ActionResult Add()
@@ -55,5 +57,23 @@ namespace Blog.Controllers
             return View();
         }
 
+
+        public ActionResult Delete(int id)
+        {
+            // get the post
+            var post = _context.Posts.FirstOrDefault(p => p.Id == id);
+
+            // check if the user is the owner of that post
+            if (post.User.Id == User.Identity.GetUserId())
+            {
+                // delete the post and redirect to Index
+                _context.Posts.Remove(post);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            // else return Unauthorized
+            return new HttpUnauthorizedResult();
+        }
     }
 }
